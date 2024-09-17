786777fa478dbd06ecca386dfb75b6cf6b53c564
Rourke Whitcombe <rourke28whit@gmail.com>
2024-09-17 15:03:52 +0000

    feat: I have added the add turn function in

--- scripts/game.js
+++ scripts/game.js
@@ -2,14 +2,20 @@ let game = {
     currentGame: [],
     playerMoves: [],
     score: 0,
-    choices: ["button1", "button2", "button3", "button4"]
+    choices: ["button1", "button2", "button3", "button4"],
 };
+
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);    
+}
 
 function newGame() {
     game.currentGame = [];
     game.playerMoves = [];
     game.score = 0;
     showScore();
+    addTurn();
 }
 
 function showScore() {
